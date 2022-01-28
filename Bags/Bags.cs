7e9de95b076fccce6742dffa893b454ef8428734
Davi Garms <davi.garms@spektrix.com>
2022-01-28 10:12:27 +0000

    initial commit

---
+++ Bags/Bags.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace Bags
+{
+    public class Bags
+    {
+        public List<string> BackPack { get; init; } = new ();
+        public List<string> ExtraBag1 { get; init; } = new ();
+        public List<string> ExtraBag2 { get; init; } = new ();
+        public List<string> ExtraBag3 { get; init; } = new ();
+        public List<string> ExtraBag4 { get; init; } = new ();
+
+        public Bags Add(string item)
+        {
+            if (BackPack.Count < 8) BackPack.Add(item);
+            else 
+            if (ExtraBag1.Count < 4) ExtraBag1.Add(item);
+            else 
+            if (ExtraBag2.Count < 4) ExtraBag2.Add(item);
+            else 
+            if (ExtraBag3.Count < 4) ExtraBag3.Add(item);
+            else 
+            if (ExtraBag4.Count < 4) ExtraBag4.Add(item);
+            
+            return this;
+        }
+        
+        public Bags Add(IEnumerable<string> items)
+        {
+            foreach (var item in items)
+            {
+                if (BackPack.Count < 8) BackPack.Add(item);
+                else 
+                if (ExtraBag1.Count < 4) ExtraBag1.Add(item);
+                else 
+                if (ExtraBag2.Count < 4) ExtraBag2.Add(item);
+                else 
+                if (ExtraBag3.Count < 4) ExtraBag3.Add(item);
+                else 
+                if (ExtraBag4.Count < 4) ExtraBag4.Add(item);
+            }
+            
+            return this;
+        }
+    }
+}
---
+++ Bags/Item.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Bags
+{
+    public class Item
+    {
+        private string _name;
+
+
+        public Item(string name = "item")
+        {
+            _name = name;
+        }
+    }
+    
+    public class Cloth : Item
+    {
+        public Cloth(string name = "cloth") : base(name){}
+    }
+    
+    public class Herb : Item
+    {
+        public Herb(string name = "herb") : base(name){}
+    }
+    
+    public class Metal : Item
+    {
+        public Metal(string name = "herb") : base(name){}
+    }
+    
+    public class Weapon : Item
+    {
+        public Weapon(string name = "herb") : base(name){}
+    }
+}
